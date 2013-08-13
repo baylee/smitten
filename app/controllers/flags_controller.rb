b2f8157487791b618263ebb42eb98d1c8d995368
Baylee Feore <baylee.feore@gmail.com>
2013-08-13 11:02:17 -0700

    Added flagging for sparks

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -4,12 +4,14 @@ class FlagsController < ApplicationController
       flagger_id: current_user.id,
       flaggable_type: params[:flag][:flaggable_type])
 
-    @conversation_partner = User.find(params[:flag][:sender_id])
-    @last_message = Message.where('sender_id = ? and receiver_id = ?', @conversation_partner.id, current_user.id)[-1]
+    if params[:flag][:sender_id]
+      @conversation_partner = User.find(params[:flag][:sender_id])
+      @last_message = Message.where('sender_id = ? and receiver_id = ?', @conversation_partner.id, current_user.id)[-1]
+      @flag.update_attribute(:flaggable_id, @last_message.id)
+    elsif params[:flag][:flagged_id]
+      @flag.update_attribute(:flaggable_id, params[:flag][:flagged_id])
+    end
 
-    @flag.update_attribute(:flaggable_id, @last_message.id)
-
-    binding.pry
     respond_to do |format|
       if @flag.save
         format.html { redirect_to messages_path, notice: 'Content was flagged'}
