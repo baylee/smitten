78b1af8b160a21bb25b87db9ded3b33811136094
Baylee Feore <baylee.feore@gmail.com>
2013-08-13 11:13:43 -0700

    Some visual cleanup and commenting

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -4,10 +4,14 @@ class FlagsController < ApplicationController
       flagger_id: current_user.id,
       flaggable_type: params[:flag][:flaggable_type])
 
+    # When a flag comes from a message,
+    # find the last message that the 'bad' sender sent to the receiver and flag that one
     if params[:flag][:sender_id]
       @conversation_partner = User.find(params[:flag][:sender_id])
       @last_message = Message.where('sender_id = ? and receiver_id = ?', @conversation_partner.id, current_user.id)[-1]
       @flag.update_attribute(:flaggable_id, @last_message.id)
+
+    # When a flag comes from a spark
     elsif params[:flag][:flagged_id]
       @flag.update_attribute(:flaggable_id, params[:flag][:flagged_id])
     end
--- app/views/flags/create.js.coffee
+++ app/views/flags/create.js.coffee
@@ -1,1 +1,7 @@
-$('#flagged').html('You flagged this conversation. You will not see any new messages.')
+$flagged_message_div = $('#flagged_message')
+$flagged_spark_div = $('#flagged_spark')
+
+if $flagged_message_div.length != 0
+  $flagged_message_div.html('You flagged this conversation. You will not see any new messages.')
+else if $flagged_spark_div.length != 0
+  $flagged_spark_div.html('Flag saved.')
