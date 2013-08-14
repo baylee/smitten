8e813047b5d608551a280b7dcd39bc9f47f680cf
Gabe <gabedominguez@outlook.com>
2013-08-14 11:28:05 -0700

    Gabes last commit before eric screws it up

--- app/assets/javascripts/swipes.js
+++ app/assets/javascripts/swipes.js
@@ -5,19 +5,23 @@ $( document ).ready(function(event) {
   var sparkbutton = $("#sparkbutton");
   var dashbutton =  $("#dashbutton");
   var mainwrapper = $("#mainwrapper");
-  var new_spark = $("#new_spark_div")
-  var homepage = $("#homepage")
-  var dashboard = $("#dashboard")
+  var new_spark = $("#new_spark_div");
+  var homepage = $("#homepage");
+  var dashboard = $("#dashboard");
 
   new_spark.swipe({
     swipeLeft:function(event, direction, distance, duration, fingerCount) {
+
       homepage.css('position', 'absolute');
       homepage.css('width', '100%');
+      $("#homepage").css('left', '100%');
       dashboard.removeAttr("style");
+      alert('foo');
       new_spark.animate({
         right:'100%',
         opacity: 1
       },{ duration: 500, queue: false});
+      $('#homepage').css("display", "block");
       $('#homepage').animate({
         left: "0%",
         opacity: 1
@@ -36,6 +40,7 @@ $( document ).ready(function(event) {
           left:'100%',
           opacity: 1
         },{ duration: 500, queue: false});
+        $('#homepage').css("display", "block");
         $('#homepage').animate({
           right: "0%",
           opacity: 1
