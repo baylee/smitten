bf6ca8ede7b5f83b90a8cfbc32152418829b42fc
Gabe <gabedominguez@outlook.com>
2013-08-14 00:05:34 -0700

    pages nicely slide together

--- app/assets/javascripts/swipes.js
+++ app/assets/javascripts/swipes.js
@@ -11,39 +11,39 @@ $( document ).ready(function(event) {
 
   new_spark.swipe({
     swipeLeft:function(event, direction, distance, duration, fingerCount) {
-      var new_spark = $("#new_spark_div")
-      var homepage = $("#homepage")
-      var dashboard = $("#dashboard")
-      homepage.css('display', 'block');
       homepage.css('position', 'absolute');
       homepage.css('width', '100%');
-      new_spark.removeAttr("style")
-      dashboard.removeAttr("style")
-      homepage.css('zindex', '1000')
+      dashboard.removeAttr("style");
+      new_spark.animate({
+        right:'100%',
+        opacity: 1
+      },{ duration: 500, queue: false});
       homepage.animate({
         left: "0%",
         opacity: 1
-      }, 500 );
+      }, { duration: 500, queue: false});
       //when you swipe on the container div new_spark, click on the homebutton link
     }
   });
   dashboard.swipe({
     swipeRight:function(event, direction, distance, duration, fingerCount) {
-
-      homepage.css('display', 'block');
       homepage.css('position', 'absolute');
       homepage.css('width', '100%');
-      new_spark.removeAttr("style")
-      dashboard.removeAttr("style")
-      homepage.css('zindex', '1000')
+      new_spark.removeAttr("style");
+      dashboard.animate({
+        left:'100%',
+        opacity: 1
+      },{ duration: 500, queue: false});
+
       homepage.animate({
         right: "0%",
         opacity: 1
-      }, 500 );
+      }, { duration: 500, queue: false} );
     }
   });
   homepage.swipe({
     swipe:function(event, direction, distance, duration, fingerCount) {
+      homepage.removeAttr('style');
       if (direction === "right"){
         $.getScript("swipe_to_spark");
 
