3f5a476f1011b394a8dd327b022ec8f1c26514a4
Eric Mooney <eric.c.mooney@gmail.com>
2013-08-14 13:53:47 -0700

    Fix ajax by adding other pages div, update height of dashboard container on dash page

--- app/assets/javascripts/swipes.js
+++ app/assets/javascripts/swipes.js
@@ -1,13 +1,11 @@
 //This is for the swipe events on the home page, dashboard, and new spark page
 
 $( document ).ready(function(event) {
-  var homebutton =  $("#homebutton");
-  var sparkbutton = $("#sparkbutton");
-  var dashbutton =  $("#dashbutton");
   var mainwrapper = $("#mainwrapper");
   var new_spark = $("#new_spark_div");
   var homepage = $("#homepage");
   var dashboard = $("#dashboard");
+  var other_pages = $("#other_pages");
 
   new_spark.swipe({
     swipeLeft:function(event, direction, distance, duration, fingerCount) {
@@ -25,7 +23,6 @@ $( document ).ready(function(event) {
         left: "0%",
         opacity: 1
       }, { duration: 500, queue: false});
-      //when you swipe on the container div new_spark, click on the homebutton link
     }
   });
   dashboard.swipe({
